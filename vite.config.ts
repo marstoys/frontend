84eba2097a0d9fd57a04c72a66ed4f15a93b72c9
Sardorbek <nsardorbek776@gmail.com>
2025-04-11 15:22:39 +0500

    front

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  publicDir: 'src',
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      'localhost',
+      'frontend-production-1d06.up.railway.app',
+      'toysmars.uz',
+      'www.toysmars.uz'
+    ]
 })
